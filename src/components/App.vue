6a4aeb6e631e49b0b5076ad195838cab683eb54c
ptiwari2 <ptiwari2@enova.com>
2020-11-02 23:02:38 -0600

    Initial set up

---
+++ src/components/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <h1>Credit report viewer</h1>
+    <h1>Credit report viewer</h1>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+h1 {
+  font-weight: normal;
+}
+</style>
